cdbf4f18cad56b5d9794e51badf9309c7c177330
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-08-23 18:13:08 -0300

    prototipo de tela Menu

---
+++ letterflix/src/paginas/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardFilmes from '../componentes/CardFilmes'
+import Navbar from '../componentes/Navbar'
+
+export default function Menu() {
+  return (
+    <div>
+        <Navbar/>
+        <CardFilmes/>
+    </div>
+  )
+}
