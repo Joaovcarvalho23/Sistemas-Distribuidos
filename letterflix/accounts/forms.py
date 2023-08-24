d1da0d823bb360cb3ad48975ac922733e077a4ca
hagafedra <haga.fedra@gmail.com>
2023-08-24 13:01:43 -0300

    feat: edição

---
+++ letterflix/accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, required=True)
+    age = forms.IntegerField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'age')
+
+class UserProfileChangeForm(UserChangeForm):
+    class Meta:
+        model = UserProfile
+        fields = ('age',)
--- letterflix/accounts/views.py
+++ letterflix/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, UserProfileChangeForm
+from .models import UserProfile
 
 @login_required  # Isso garante que o usuário esteja autenticado para acessar essa página
 def dashboard(request):
@@ -10,13 +12,18 @@ def dashboard(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('login')  # Redirecionar para a página de login após o cadastro
+            user = form.save(commit=False)  # Não salve o usuário ainda
+            age = form.cleaned_data['age']  # Obtenha a idade do formulário
+            user.save()
+
+            # Agora que o usuário foi salvo, crie o UserProfile associado com a idade
+            UserProfile.objects.create(user=user, age=age)
+
+            return redirect('dashboard')  # Redirecione para a página do usuário após o cadastro
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 def login(request):
@@ -35,3 +42,17 @@ def logout(request):
         auth_logout(request)
         return redirect('login')  # Redirecione para a página inicial após o logout
     return render(request, 'accounts/logout.html')
+
+@login_required
+def edit_profile(request):
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:dashboard')
+    else:
+        form = UserProfileChangeForm(instance=request.user)
+    
+    return render(request, 'accounts/edit_profile.html', {'form': form})
