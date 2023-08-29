027bae557ddbb8d0df822f6411be235c7f8dd16b
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-08-28 23:12:49 -0300

    perfilFilmes retorna os dados do filme

--- letterflix/src/paginas/Menu.jsx
+++ letterflix/src/paginas/Menu.jsx
@@ -30,7 +30,7 @@ export default function Menu({setDadosFilmes}) {
 
   const getFilmes = () => {
     axios
-      .get('https://api.themoviedb.org/3/movie/popular?api_key=332bf3a464fa6e428a995701bd2d29f2&language=pt-US&page=1')
+      .get('https://api.themoviedb.org/3/movie/popular?api_key=332bf3a464fa6e428a995701bd2d29f2&language=pt-BR&page=1')
       .then((result) => setFilmes(result.data.results))
       .catch((err) => console.log(err));
   };
@@ -38,8 +38,8 @@ export default function Menu({setDadosFilmes}) {
   const moviePickHandler = (filmeData) =>{
     setDadosFilmes(filmeData)
     navigate('/perfilFilmes')
+    console.log(filmeData)
   }
-
   return (
     <div>
       <Navbar setFiltro={setFiltro} />
@@ -49,7 +49,7 @@ export default function Menu({setDadosFilmes}) {
           {filmes.filter((filme) => filme.title.toLowerCase().includes(filtro.toLowerCase())).map((filme) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={filme.id}>
 
-              <Box onClick ={() => moviePickHandler(filme.data)}>
+              <Box onClick ={() => moviePickHandler(filme)}>
                 <CardFilmes
                   nome={filme.title}
                   dataEstreia={filme.release_date}
