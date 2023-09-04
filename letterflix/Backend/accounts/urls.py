cbe2110ee68c6064fbb1f7059b55cbbbd2e50990
hagafedra <haga.fedra@gmail.com>
2023-09-04 19:40:36 -0300

    fix: remove dashboard

--- letterflix/Backend/accounts/urls.py
+++ letterflix/Backend/accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
