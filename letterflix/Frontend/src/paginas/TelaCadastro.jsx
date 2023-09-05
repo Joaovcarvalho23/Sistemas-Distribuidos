b8f5582576e7b97c4075a911e1e14da8d1d4ba97
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-09-04 21:07:37 -0300

    estilizando a tela de cadastro

--- letterflix/Frontend/src/paginas/TelaCadastro.jsx
+++ letterflix/Frontend/src/paginas/TelaCadastro.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Container, CssBaseline, Typography, TextField, Button, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const TelaCadastro = () => {
@@ -7,7 +8,7 @@ const TelaCadastro = () => {
   const [formData, setFormData] = useState({
     username: '',
     password: '',
-    age: '', // Adicione campos adicionais conforme necessário
+    age: '',
   });
 
   const handleInputChange = (e) => {
@@ -28,51 +29,80 @@ const TelaCadastro = () => {
 
     try {
       await axios.post('http://127.0.0.1:8000/accounts/signup/', formData).then((response) => {
-        navigate('/')
-        console.log(response.data)
+        navigate('/');
+        console.log(response.data);
       });
-      } catch (error) {
+    } catch (error) {
       console.error(error);
     }
   };
 
-
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
+    <Paper
+      sx={{
+        backgroundImage: 'url("/assets/fotoCinema.jpg")', // Adicione o caminho da sua imagem de fundo
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh', // Altura mínima da tela inteira
+        padding: '2rem',
+      }}
+    >
+      <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <img
+          src="/assets/LetterFlixLogo.png"
+          alt="Logo" // Adicione um alt para acessibilidade
+          style={{ width: '420px', height: '200px', marginBottom: '1rem' }} // Largura e altura personalizadas
+        />
+        <Typography component="h1" variant="h5" sx={{ textAlign: 'center', color: 'white' }}>
+          Bem-vindo(a) ao LetterFlix! O aplicativo com os filmes que estão bombando hoje nos cinemas!
+        </Typography>
+        <form onSubmit={handleFormSubmit} style={{ width: '100%' }}>
+          <TextField
+            variant="filled"
+            margin="normal"
+            required
+            fullWidth
+            label="Crie seu usuário"
             name="username"
             value={formData.username}
             onChange={handleInputChange}
+            sx={{ backgroundColor: 'white' }} // Fundo branco
           />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
+          <TextField
+            variant="filled"
+            margin="normal"
+            required
+            fullWidth
+            label="Crie sua senha"
             type="password"
-            id="password"
             name="password"
             value={formData.password}
             onChange={handleInputChange}
+            sx={{ backgroundColor: 'white' }} // Fundo branco
           />
-        </div>
-        <div>
-          <label htmlFor="age">Age:</label>
-          <input
+          <TextField
+            variant="filled"
+            margin="normal"
+            required
+            fullWidth
+            label="Informe sua idade"
             type="number"
-            id="age"
             name="age"
             value={formData.age}
             onChange={handleInputChange}
+            sx={{ backgroundColor: 'white' }} // Fundo branco
           />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
-    </div>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+            Cadastrar
+          </Button>
+        </form>
+      </Container>
+    </Paper>
   );
 };
 
